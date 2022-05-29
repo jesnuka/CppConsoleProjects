527c92170dcdf5949511785d9d49bb19a85f2843
Jesse Nukarinen <jesse.nukarinen@aalto.fi>
2022-05-29 23:11:01 +0900

    Add PhysicsObject class and Dummy class. Add drawing and physics calculation to objects.

--- CppConsoleProjects/Worms.cpp
+++ CppConsoleProjects/Worms.cpp
@@ -17,6 +17,22 @@
 
 #include "Worms.h"
 
+vector<pair<float, float>> DefineDummy()
+{
+	vector<pair<float, float>> model;
+
+	// Produce list of points in an unit circle
+	model.push_back({ 0.0f, 0.0f });
+	for (int i = 0; i < 10; i++)
+		model.push_back({ cosf(i / 9.0f * 2.0f * M_PI), sinf(i / 9.0f * 2.0f * M_PI) });
+
+	return model;
+}
+
+// Factory function for creation
+vector<pair<float, float>> Dummy::model = DefineDummy();
+
+
 void Worms::CreateMap()
 {
 	// Create 1D Perlin Noise
@@ -50,7 +66,7 @@ void Worms::CreateMap()
 	delete[] noiseSeed;
 }
 
-void Worms::PerlinNoise1D(int count, float* seed, int octaves, float bias, float* output)
+void Worms::PerlinNoise1D(int count, float *seed, int octaves, float bias, float *output)
 {
 	// 1D Perlin Noise
 	for (int x = 0; x < count; x++)
@@ -61,8 +77,11 @@ void Worms::PerlinNoise1D(int count, float* seed, int octaves, float bias, float
 
 		for (int o = 0; o < octaves; o++)
 		{
+			// Divide pitch by two
 			int pitch = count >> o;
+			// Get sample point, division and multiplication are because of the loss of information caused by integers
 			int sample1 = (x / pitch) * pitch;
+			// Loop around, allow for tesselation
 			int sample2 = (sample1 + pitch) % count;
 
 			float blend = (float)(x - sample1) / (float)pitch;
@@ -71,6 +90,8 @@ void Worms::PerlinNoise1D(int count, float* seed, int octaves, float bias, float
 
 			scaleAcc += scale;
 			noise += sample * scale;
+
+			// Scale is divided as the value decreases each octave
 			scale = scale / bias;
 		}
 
@@ -96,6 +117,12 @@ bool Worms::OnUserUpdate(float elapsedTime)
 	if (keys[L'M'].isReleased)
 		CreateMap();
 
+	if (mouse[2].isReleased)
+	{
+		Dummy *dummy = new Dummy(mousePositionX + cameraPosX, mousePositionY + cameraPosY);
+		objects.push_back(dummy);
+	}
+
 	// Sides of the screen allow for moving the camera around the map
 	if (mousePositionX < 5) 
 		cameraPosX -= mapScrollSpeed * elapsedTime;
@@ -116,8 +143,34 @@ bool Worms::OnUserUpdate(float elapsedTime)
 	if (cameraPosY >= mapHeight - screenHeight) 
 		cameraPosY = mapHeight - screenHeight;
 
+	// Update physics of physics objects
+	for (auto &o : objects)
+	{
+		// Apply Gravity to object
+		o->accelY += 9.81f;
+
+		// Update Velocity in respect to acceleration
+		o->velocityX += o->accelX * elapsedTime;
+		o->velocityY += o->accelY * elapsedTime;
+
+		// Update position according to the velocity
+		// However, use potential variables because collision needs to be tested first
+		float potentialPosX = o->posX + o->velocityX * elapsedTime;
+		float potentialPosY = o->posY + o->velocityY * elapsedTime;
+
+		// Reset acceleration at the end
+		o->accelX = 0.0f;
+		o->accelY = 0.0f;
+		o->stable = false;
+
+		o->posX = potentialPosX;
+		o->posY = potentialPosY;
+
+	}
+
 	// Empty screen
 	Fill(0, 0, screenWidth, screenHeight, L' ', 0);
+
 
 	// Draw the Landscape
 	for (int i = 0; i < screenWidth; i++)
@@ -130,7 +183,7 @@ bool Worms::OnUserUpdate(float elapsedTime)
 				Draw(i, j, PIXEL_FULL, FG_CYAN);
 				break;
 			case 1:
-				Draw(i, j, PIXEL_FULL, FG_GREEN);
+				Draw(i, j, PIXEL_FULL, FG_DARK_GREEN);
 				break;
 			default:
 				Draw(i, j, PIXEL_FULL, FG_CYAN);
@@ -138,6 +191,10 @@ bool Worms::OnUserUpdate(float elapsedTime)
 			}
 		}
 	}
+
+	// Draw all the objects using each objects own Draw method
+	for (auto& o : objects)
+		o->Draw(this, cameraPosX, cameraPosY);
 
     return true;
 }
--- CppConsoleProjects/Worms.h
+++ CppConsoleProjects/Worms.h
@@ -20,6 +20,49 @@
 #include "ConsoleEngine.h"
 using namespace std;
 
+class PhysicsObject
+{
+public:
+	float posX = 0.0f;
+	float posY = 0.0f;
+	float velocityX = 0.0f;
+	float velocityY = 0.0f;
+	float accelX = 0.0f;
+	float accelY = 0.0f;
+
+	float radius = 4.0f;
+	// Stable objects do not move, and don't need to have more calculations
+	bool stable = false;
+
+	PhysicsObject(float x = 0.0f, float y = 0.0f)
+	{
+		posX = x;
+		posY = y;
+	}
+
+	virtual void Draw(ConsoleEngine *engine, float offsetX, float offsetY) = 0;
+};
+
+class Dummy : public PhysicsObject
+{
+public:
+	Dummy(float x= 0.0f, float y = 0.0f) : PhysicsObject(x,y)
+	{
+
+	}
+
+	virtual void Draw(ConsoleEngine *engine, float offsetX, float offsetY)
+	{
+		// Draw wireframe model of dummy, 
+		// Position is offset by camera, atan2f to get the angle using velocities, Scale is done using radius to see the bounding radius
+		engine->WireFrame(model, posX - offsetX, posY - offsetY, atan2f(velocityY, velocityX),radius, PIXEL_FULL, FG_WHITE);
+	}
+
+private:
+	// Share model across models of the same class
+	static vector<pair<float, float>> model;
+};
+
 class Worms : public ConsoleEngine
 {
 public:
@@ -27,6 +70,7 @@ public:
 	{
 		appName = L"Worms";
 	}
+
 
 private:
 
@@ -38,6 +82,10 @@ private:
 	float cameraPosX = 0.0f;
 	float cameraPosY = 0.0f;
 	float mapScrollSpeed = 200.0f;
+
+	// List of all the physics objects, as pointers because they are abstract
+	// Subclasses can therefore also be added to this list
+	list<PhysicsObject*> objects;
 
 	unsigned char* map = nullptr;
 
